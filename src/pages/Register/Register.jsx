154062985e4533602b4277d6f426f79d70693025
emonShimoul <emonshimoul@gmail.com>
2025-03-12 01:37:47 +0600

    use of lottie react

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,32 @@
+import Lottie from "lottie-react";
+import React from "react";
+import registerLottieData from "../../assets/register.json";
+
+const Register = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left w-1/3">
+          <Lottie animationData={registerLottieData}></Lottie>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <h1 className="text-4xl font-bold ml-6 mt-6">Register now!</h1>
+          <form className="card-body">
+            <fieldset className="fieldset">
+              <label className="fieldset-label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+              <label className="fieldset-label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Register</button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
