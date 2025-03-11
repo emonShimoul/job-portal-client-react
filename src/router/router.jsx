52a1c42e9a50fb0d83e246a9b69b96d1b7fbd865
emonShimoul <emonshimoul@gmail.com>
2025-03-12 01:21:31 +0600

    created a navbar

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
+    errorElement: <h2>Route Not Found</h2>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
