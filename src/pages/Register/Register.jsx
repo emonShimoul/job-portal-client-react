3ed70aabe66d269ac785ec6b6deacf7ab65a4205
emonShimoul <emonshimoul@gmail.com>
2025-03-12 10:44:46 +0600

    setup firebase and context api

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,6 +3,15 @@ import React from "react";
 import registerLottieData from "../../assets/register.json";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+
+    // TODO: Password validation
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,12 +20,22 @@ const Register = () => {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <h1 className="text-4xl font-bold ml-6 mt-6">Register now!</h1>
-          <form className="card-body">
+          <form onSubmit={handleRegister} className="card-body">
             <fieldset className="fieldset">
               <label className="fieldset-label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+              />
               <label className="fieldset-label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input
+                type="password"
+                name="password"
+                className="input"
+                placeholder="Password"
+              />
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
