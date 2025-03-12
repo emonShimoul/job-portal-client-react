b6d927f59dc2594f78dfe47a384a077607788836
emonShimoul <emonshimoul@gmail.com>
2025-03-12 12:55:47 +0600

    banner image animation using framer motion

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { motion } from "motion/react";
 import { easeOut } from "motion";
+import team1 from "../../assets/team/team1.jpg";
+import team2 from "../../assets/team/team2.jpg";
 
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="flex-1">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            className="max-w-sm rounded-lg shadow-2xl"
+          <motion.img
+            src={team2}
+            animate={{ y: [50, 100, 50] }}
+            transition={{ duration: 8, repeat: Infinity }}
+            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-400 shadow-2xl"
+          />
+          <motion.img
+            src={team1}
+            animate={{ x: [100, 150, 100] }}
+            transition={{ duration: 8, repeat: Infinity }}
+            className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px] border-l-4 border-b-4 border-blue-400 shadow-2xl"
           />
         </div>
         <div className="flex-1">
@@ -23,7 +33,14 @@ const Banner = () => {
             }}
             className="text-5xl font-bold"
           >
-            Latest Jobs for You!
+            Latest{" "}
+            <motion.span
+              animate={{ color: ["#ecff33", "#33ffe3"] }}
+              transition={{ duration: 1.5, repeat: Infinity }}
+            >
+              Jobs
+            </motion.span>{" "}
+            for You!
           </motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
