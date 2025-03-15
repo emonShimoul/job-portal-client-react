b5a426028189bafff41f59de204dd70c0fd22439
emonShimoul <emonshimoul@gmail.com>
2025-03-16 02:13:31 +0600

    create large form with select for Add Form

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -14,6 +14,7 @@ const AddJob = () => {
             className="input w-full"
             placeholder="Job Title"
           />
+
           {/* job location */}
           <label className="fieldset-label">Job Location</label>
           <input
@@ -22,7 +23,125 @@ const AddJob = () => {
             className="input w-full"
             placeholder="Job Location"
           />
-          <button className="btn btn-neutral mt-4">Login</button>
+
+          {/* Job type */}
+          <label className="fieldset-label">Job Type</label>
+          <select defaultValue="Pick a font" className="select select-ghost">
+            <option disabled={true}>Pick a Job Type</option>
+            <option>Full Time</option>
+            <option>Part Time</option>
+            <option>Intern</option>
+          </select>
+
+          {/* Job category */}
+          <label className="fieldset-label">Job Field</label>
+          <select defaultValue="Pick a font" className="select select-ghost">
+            <option disabled={true}>Pick a Job Field</option>
+            <option>Engineering</option>
+            <option>Marketing</option>
+            <option>Finance</option>
+            <option>Teaching</option>
+          </select>
+
+          {/* salary range */}
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 items-end">
+            <div>
+              <label className="fieldset-label">Salary Range</label>
+              <input
+                type="text"
+                name="min"
+                className="input w-full"
+                placeholder="Min"
+              />
+            </div>
+
+            <div>
+              <input
+                type="text"
+                name="max"
+                className="input w-full"
+                placeholder="Max"
+              />
+            </div>
+
+            <div>
+              <select
+                defaultValue="Pick a font"
+                className="select select-ghost"
+              >
+                <option disabled={true}>Pick a Currency</option>
+                <option>BDT</option>
+                <option>USD</option>
+                <option>INR</option>
+              </select>
+            </div>
+          </div>
+
+          {/* job description */}
+          <label className="fieldset-label">Job Description</label>
+          <textarea
+            className="textarea"
+            name="description"
+            placeholder="Job Description"
+            required
+          ></textarea>
+
+          {/* Company name */}
+          <label className="fieldset-label">Company Name</label>
+          <input
+            type="text"
+            name="company"
+            className="input w-full"
+            placeholder="Company Name"
+          />
+
+          {/* job requirements */}
+          <label className="fieldset-label">Job Requirements</label>
+          <textarea
+            className="textarea"
+            name="requirements"
+            placeholder="Put each requirements in a new line"
+            required
+          ></textarea>
+
+          {/* job responsibilities */}
+          <label className="fieldset-label">Job Responsibilities</label>
+          <textarea
+            className="textarea"
+            name="responsibilities"
+            placeholder="Put each responsibilities in a new line"
+            required
+          ></textarea>
+
+          {/* HR email */}
+          <label className="fieldset-label">HR Email</label>
+          <input
+            type="email"
+            name="hr_email"
+            className="input w-full"
+            placeholder="HR Email"
+          />
+
+          {/* HR name */}
+          <label className="fieldset-label">HR Name</label>
+          <input
+            type="text"
+            name="hr_name"
+            className="input w-full"
+            placeholder="HR Name"
+          />
+
+          {/* Company logo */}
+          <label className="fieldset-label">Company Logo</label>
+          <input
+            type="text"
+            name="company_logo"
+            className="input w-full"
+            placeholder="Company Logo"
+          />
+
+          {/* Submit button */}
+          <button className="btn btn-neutral mt-4">Submit</button>
         </form>
       </div>
     </div>
